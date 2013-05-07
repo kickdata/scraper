0ae3b3eff0168ef18cff07c9496c473c22bcb599
Andreas Taube <andreas.taube@t-systems.com>
2013-05-07 17:38:00 +0200

    using async and child process for batch processing

---
+++ importer/import.js
@@ -0,0 +1,113 @@
+var $ = require('jquery');
+var fs = require('fs');
+var async = require('async');
+var moment = require('moment');
+var MongoClient = require('mongodb').MongoClient;
+
+function getProjectStatus(data) {
+  var selector = $('#main_content', data).attr('class');
+  var state = 'unknown';
+
+  if(selector.indexOf('-state-successful') > 0) {
+    state = 'successful';
+  }
+
+  if(selector.indexOf('-state-live') > 0) {
+    state = 'live';
+  }
+
+  if(selector.indexOf('-state-failed') > 0) {
+    state = 'failed';
+  }
+  return state;
+}
+
+function getBackers(data) {
+  return $('.NS_backers__backing_row', data).map(function() {
+    var b = {};
+    b.name = $('.meta h5 a', this).text();
+    b.profile = $('.meta h5 a', this).attr('href');
+    b.location = $('.location', this).text().trim();
+    return b;
+  });
+}
+
+function getPledges(data) {
+  var result = [];
+
+  $('#what-you-get li', data).each(function() {
+    var p = {};
+    p.pledge=$('h5', this).text().substring(8).split(' ')[0];
+    p.unit = $('h5', this).text().substring(7, 8);
+    p.backers_count= $('.num-backers', this).text().trim().split(' ')[0];
+    result.push(p);
+  });
+  return result;
+}
+
+function parseHtml(file, callback) {
+    var fileContent = fs.readFileSync(file, {'encoding': 'utf-8'});
+    var data = $(fileContent);
+
+    var project = {
+      title: $('#title a',data).text(),
+      creator: $('.creator', data).text().trim().substring(3),
+      category: $('.category', data).attr('data-project-parent-category'),
+      location: $('.location:first a', data).text().trim(),
+      start: moment($('.posted', data).text().trim().substring(10)).toJSON(),
+      end: moment($('.ends', data).text().trim().substring(10)).toJSON(),
+      last_update: moment().toJSON(),
+      pledged: {
+        goal: $('#pledged', data).attr('data-goal'),
+        percent_raised: $('#pledged', data).attr('data-percent-raised'),
+        pledged: $('#pledged', data).attr('data-pledged'),
+        currency: $('#pledged data', data).attr('data-currency')
+      },
+      status: getProjectStatus(data),
+      pledges: getPledges(data)
+    };
+
+    //TODO
+    // var backersFileContent = fs.readFileSync(file.replace(path.extname(file), '') + '__backers.html', {'encoding': 'utf-8'});
+    // project.backers = getBackers($(backersFileContent));
+    callback(project);
+}
+
+function importProjects(projects, callback) {
+  MongoClient.connect("mongodb://localhost/kickdata", function(err, db) {
+    if(err) { return console.dir(err); }
+
+    var collection = db.collection('projects');
+
+    collection.insert(projects, function(err) {
+
+      if(err) {
+        console.dir(err);
+      }
+      console.log('Done');
+      callback();
+    });
+  });
+}
+
+process.on('message', function(m) {
+
+  var projects = [];
+
+  console.log('Batch ' + m.index);
+
+  async.eachSeries(m.chunk, function(item, callback) {
+    parseHtml(item, function(obj) {
+      console.log('  Importing ' + item);
+      projects.push(obj);
+    });
+
+    callback();
+
+  }, function() {
+    importProjects(projects, function() {
+      process.send({status: 'success'});
+      process.disconnect();
+    });
+  });
+});
--- importer/index.js
+++ importer/index.js
@@ -1,9 +1,9 @@
 var _ = require('underscore');
-var $ = require('jquery');
 var fs = require('fs');
 var path = require('path');
-var moment = require('moment');
-var MongoClient = require('mongodb').MongoClient;
+var async = require('async');
+var cp = require('child_process');
+
 
 var dataDir = 'data';
 
@@ -25,141 +25,58 @@ Array.prototype.chunk = function(chunkSize) {
     );
 };
 
-function getProjectStatus(data) {
-  var selector = $('#main_content', data).attr('class');
-  var state = 'unknown';
-
-  if(selector.indexOf('-state-successful') > 0) {
-    state = 'successful';
-  }
-
-  if(selector.indexOf('-state-live') > 0) {
-    state = 'live';
-  }
-
-  if(selector.indexOf('-state-failed') > 0) {
-    state = 'failed';
-  }
-  return state;
-}
-
-function getBackers(data) {
-  return $('.NS_backers__backing_row', data).map(function() {
-    var b = {};
-    b.name = $('.meta h5 a', this).text();
-    b.profile = $('.meta h5 a', this).attr('href');
-    b.location = $('.location', this).text().trim();
-    return b;
-  });
-}
-
-function getPledges(data) {
-  var result = [];
-
-  $('#what-you-get li', data).each(function() {
-    var p = {};
-    p.pledge=$('h5', this).text().substring(8).split(' ')[0];
-    p.unit = $('h5', this).text().substring(7, 8);
-    p.backers_count= $('.num-backers', this).text().trim().split(' ')[0];
-    result.push(p);
-  });
-  return result;
-}
-
 function getHtmlFiles(dataDir, callback) {
   fs.readdir(dataDir, function(err,files) {
     var htmlFiles = _.reject(files,function(f) { return !f.endsWith('.html') || f.endsWith('__backers.html');} );
     htmlFiles = _.map(htmlFiles, function(file) {
       return path.join(dataDir,file);
     });
-    callback(htmlFiles);
+    callback(htmlFiles.slice(0, 200));
   });
-}
-
-function parseHtml(file, callback) {
-    var fileContent = fs.readFileSync(file, {'encoding': 'utf-8'});
-    var data = $(fileContent);
-
-    var project = {
-      title: $('#title a',data).text(),
-      creator: $('.creator', data).text().trim().substring(3),
-      category: $('.category', data).attr('data-project-parent-category'),
-      location: $('.location:first a', data).text().trim(),
-      start: moment($('.posted', data).text().trim().substring(10)).toJSON(),
-      end: moment($('.ends', data).text().trim().substring(10)).toJSON(),
-      last_update: moment().toJSON(),
-      pledged: {
-        goal: $('#pledged', data).attr('data-goal'),
-        percent_raised: $('#pledged', data).attr('data-percent-raised'),
-        pledged: $('#pledged', data).attr('data-pledged'),
-        currency: $('#pledged data', data).attr('data-currency')
-      },
-      status: getProjectStatus(data),
-      pledges: getPledges(data)
-    };
-
-    //TODO
-    // var backersFileContent = fs.readFileSync(file.replace(path.extname(file), '') + '__backers.html', {'encoding': 'utf-8'});
-    // project.backers = getBackers($(backersFileContent));
-    callback(project);
-}
-
-function importProject() {
-  MongoClient.connect("mongodb://localhost/kickdata", function(err, db) {
-    if(err) { return console.dir(err); }
-
-    var collection = db.collection('projects');
-
-    collection.insert(project, function(err) {
-
-      if(err) {
-        console.dir(err);
-        process.exit();
-      }
-      console.log('Done');
-    });
-
-    if(obj.index === obj.totalFiles) {
-      db.close();
-    }
-
-  });
-
 }
 
 function createChunks(files, callback) {
-  var htmlFiles = _.reject(files,function(f) { return !f.endsWith('.html') || f.endsWith('__backers.html');} );
-  _.each(htmlFiles.chunk(100), function(part, index) {
+  _.each(files.chunk(100), function(part, index) {
     callback(part, index);
   });
 }
 
+function importProject(chunk, index, callback) {
+  
+  var n = cp.fork(__dirname + '/import.js');
+  n.send({ chunk: chunk, index: index });
+
+  n.on('message', function(m) {
+    console.log('PARENT got message:', m);
+    callback();
+  });
+}
+
+var tasks = [];
 
 getHtmlFiles(dataDir, function(files) {
 
   createChunks(files, function(chunk, index) {
-
-    console.log('Chunk ' + index);
-
-    _.each(chunk, function(file) {
-
-        console.log('Importing ' + file);
-
-    //     parseHtml(file, function(project) {
-
-    //     });
-    });
+       tasks.push(function(callback) {
+          importProject(chunk, index, function() {
+            callback();
+          });
+       });
+  });
+  
+  async.waterfall(tasks, function(err, result) {
+    console.log('Finished');
+      if(err) {
+        console.log('Err:' + err);
+      }
+      process.exit();
 
   });
 
-
-
-
-  // console.log("Importing " + obj.file + ' (' + obj.index + '/' + obj.totalFiles + ')' );
-
-  
-
 });
+
+
+
 
 
 
